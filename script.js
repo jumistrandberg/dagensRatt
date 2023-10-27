9ae4d43f08a689f0092f122c202ce48d2c1fcb6e
Caroline Edgren <caroline.edgren88@gmail.com>
2023-10-27 11:05:51 +0200

    semantic changes

---
+++ script.js
@@ -0,0 +1,12 @@
+const btn = document.getElementById("sendDish");
+btn.addEventListener("click", createMenu);
+function createMenu(event){ 
+    // Stoppar refresh vid knapptryck
+event.preventDefault();
+// 1. Hämta data från ID för att skapa lokala variabler
+
+// 2. Skapa element för alla variabler
+
+// 3. Alla nya element publiseras via appendChild till diven
+}
+
